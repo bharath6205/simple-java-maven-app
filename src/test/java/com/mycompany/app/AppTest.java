9cec6e9749d085e8983a2a50e74f0e10b8db8500
bharath6205 <107345246+bharath6205@users.noreply.github.com>
2022-11-08 07:38:54 +0000

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -34,9 +34,9 @@ public class AppTest
     {
         App.main(null);
         try {
-            assertEquals("Hello Git!" + System.getProperty("line.separator"), outContent.toString());
+            assertEquals("Hello World!" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
-            fail("\"message\" is not \"Hello Git!\"");
+            fail("\"message\" is not \"Hello World!\"");
         }
     }
 
